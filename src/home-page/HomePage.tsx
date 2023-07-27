ccfa0b844d4a391e1eccf2b14517094c7742a0a2
krishna kumar kushwaha <krishnakushwaha743@gmail.com>
2023-07-27 18:39:18 +0530

    created table files

---
+++ src/home-page/HomePage.tsx
@@ -0,0 +1,104 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Button from "@mui/material/Button";
+// import CameraIcon from "@mui/icons-material/PhotoCamera";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Grid";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Link from "@mui/material/Link";
+import {createTheme, ThemeProvider} from "@mui/material/styles";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// TODO remove, this demo shouldn't need to reset the theme.
+const defaultTheme = createTheme();
+
+export default function HomePage() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          {/* <CameraIcon sx={{mr: 2}} /> */}
+          <Typography variant="h6" color="inherit" noWrap>
+            Sunday Labs
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        {/* Hero unit */}
+        <Box
+          sx={{
+            bgcolor: "background.paper",
+            pt: 8,
+            pb: 6,
+          }}
+        >
+          <Container maxWidth="sm">
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="text.primary"
+              gutterBottom
+            >
+              Hi, There
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="text.secondary"
+              paragraph
+            >
+              Something short and leading about the collection below—its
+              contents, the creator, etc. Make it short and sweet, but not too
+              short so folks don&apos;t simply skip over it entirely.
+            </Typography>
+            <Stack
+              sx={{pt: 4}}
+              direction="row"
+              spacing={4}
+              justifyContent="center"
+            >
+              <Button
+                variant="contained"
+                color="success"
+                sx={{width: "200px", height: "60px", fontSize: "19px"}}
+              >
+                Table page
+              </Button>
+              <Button
+                variant="contained"
+                color="secondary"
+                sx={{width: "200px", height: "60px", fontSize: "19px"}}
+              >
+               Graph page
+              </Button>
+            </Stack>
+          </Container>
+        </Box>
+      </main>
+    </ThemeProvider>
+  );
+}
